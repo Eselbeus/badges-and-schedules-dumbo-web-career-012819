f13c94cf08402431c4c364aeb6d03d13933d00ce
eselbeus <eselbeus@gmail.com>
2019-01-08 22:03:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is Arel"
+end
+
+def batch_badge_creator(names_arr)
+  badges = []
+  names_arr.each do |name|
+    badges << "Hello, my name is #{name}."
+  end
+end
