383cb4e32b8da2b057e073891b4cf601ff5c1f6f
eselbeus <eselbeus@gmail.com>
2019-01-08 22:11:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 def badge_maker(name)
-  "Hello, my name is Arel"
+  "Hello, my name is Arel."
 end
 
 def batch_badge_creator(names_arr)
@@ -14,7 +14,7 @@ end
 def assign_rooms(speaker)
   assignements = []
   speaker.each_with_index do |name, idx|
-    assignements << "Hello, #{name}! You'll be assigned to room #{idx}!"
+    assignements << "Hello, #{name}! You'll be assigned to room #{idx + 1}!"
   end
   return assignements
 end
