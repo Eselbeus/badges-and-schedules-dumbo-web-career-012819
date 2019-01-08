85ec2b0326670bd4452a8ba482dd9de1f041313e
eselbeus <eselbeus@gmail.com>
2019-01-08 22:08:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,4 +8,19 @@ def batch_badge_creator(names_arr)
   names_arr.each do |name|
     badges << "Hello, my name is #{name}."
   end
+  return badges
+end
+
+def assign_rooms(speaker)
+  assignements = []
+  speaker.each_with_index do |name, idx|
+    assignements << "Hello, #{name}! You'll be assigned to room #{idx}!"
+  end
+  return assignements
+end
+
+def printer
+  badges.each do |badge|
+    puts badge
+  end
 end
